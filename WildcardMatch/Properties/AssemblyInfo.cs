9de1523f366fec55e0ff303e140a333112732e38
picrap <pascal@craponne.fr>
2015-04-28 15:35:16 +0200

    Fix for nuspec

--- WildcardMatch/Properties/AssemblyInfo.cs
+++ WildcardMatch/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@
 using System.Reflection;
 
 [assembly: AssemblyDescription("String wildcard match (I can't believe no one wrote already a simple NuGet package to do this).")]
-[assembly: AssemblyCompany("craponne.fr")]
+[assembly: AssemblyCompany("picrap")]
 [assembly: AssemblyProduct("WildcardMatch")]
 [assembly: AssemblyCopyright("MIT license http://opensource.org/licenses/MIT")]
 [assembly: AssemblyTrademark("")]
